e6a274bc5efd1a102a916188d8c6a0cfd6c862cd
Bal Bahadur Bhandari <148877314+balbdr@users.noreply.github.com>
2026-08-30 15:05:18 +0545

    Refactor temperature conversion and time display
    Updated temperature conversion to show two decimal places.

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ let far = document.getElementById('far');
 cel.addEventListener('input',function(){
     let c = this.value;
     let f= (c*9/5)+32;
-    far.value = f ;
+    far.value = f.toFixed(2) ;
    
 
 })
 far.addEventListener('input',function(){
     let f =this.value;
     let c=(f-32)*5/9;
-    cel.value =c;
+    cel.value = c.toFixed(2);
 })
 
 function changecolor(){
